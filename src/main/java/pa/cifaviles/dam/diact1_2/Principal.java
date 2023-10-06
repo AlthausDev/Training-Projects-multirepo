0974755c85892f5f58df2219a9b27ae523d56c4c
Samuel Althaus <MIR80329@educastur.es>
2023-10-06 20:00:29 +0200

    New class alumno and Principal

--- src/main/java/pa/cifaviles/dam/diact1_2/Principal.java
+++ src/main/java/pa/cifaviles/dam/diact1_2/Principal.java
@@ -1,16 +1,18 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 package pa.cifaviles.dam.diact1_2;
 
 /**
  *
  * @author samuelaa
  */
-public class DIAct1_2 {
-
+public class Principal {
+    
+    private static Alumno [] alumnos = new Alumno [5];
+    
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
     }
 }
