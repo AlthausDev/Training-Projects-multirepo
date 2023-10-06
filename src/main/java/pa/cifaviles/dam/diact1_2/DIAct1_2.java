bd3fbf6180d9532cfc772180210815ec083d2530
Samuel Althaus <MIR80329@educastur.es>
2023-10-06 19:06:01 +0200

    Init

---
+++ src/main/java/pa/cifaviles/dam/diact1_2/DIAct1_2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package pa.cifaviles.dam.diact1_2;
+
+/**
+ *
+ * @author samuelaa
+ */
+public class DIAct1_2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
