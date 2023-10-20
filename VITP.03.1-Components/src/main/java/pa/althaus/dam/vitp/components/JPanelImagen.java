2664431b61eef2c350831592e7b563e5f59798c6
Samuel Althaus <MIR80329@educastur.es>
2023-10-20 17:05:35 +0200

    Initial Commit

---
+++ VITP.03.1-Components/src/main/java/pa/althaus/dam/vitp/components/JPanelImagen.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pa.althaus.dam.vitp.components;
+
+import java.io.Serializable;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author samuelaa
+ */
+public class JPanelImagen extends JPanel implements Serializable {
+
+    public JPanelImagen() {
+    }   
+    
+    
+}
