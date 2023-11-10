98e42f0db208463909d740132db5ef7a3c825833
AlthausDev <samuelalthaus@gmail.com>
2023-11-10 20:03:15 +0100

    feat(Add-Validators):

--- src/main/java/pa/althaus/dam/javaproyect/aeropuerto/validator/Validador.java
+++ src/main/java/pa/althaus/dam/javaproyect/aeropuerto/validator/Validador.java
@@ -1,51 +1,62 @@
 package pa.althaus.dam.javaproyect.aeropuerto.validator;
 
+import java.text.Format;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * @author samuelaa
  */
 public class Validador {
 
-    public boolean prefixValid(int prefix) {
+    public boolean isPrefixValid(int prefix) {
         return prefix <= 999 && prefix >= 0;
     }
 
-    public boolean comCodValid(String code) {
-        //codigo toUpperCase()
-        return code.length() == 2 && code.matches("[A-Z][A-Z0-9]");
+    public boolean isCompanyCodValid(String code) {
+        return code.length() == 2 && code.toUpperCase().matches("[A-Z][A-Z0-9]");
     }
 
-    public boolean tlfValid(String tlf) {
+    public boolean isTlfValid(String tlf) {
+        return tlf.matches("[0-9]{4,15}");
+    }
 
-        if (tlf.length() > 15) {
-            return false;
+    public boolean isFlyCodValid(String code) {
+
+        if (isCompanyCodValid(code.substring(0, 1))) {
+            String codVuelo = code.substring(2, code.length());
+            return codVuelo.matches("[%d]{1,4}");
         } else {
-            //2 partes, la primera un String de 3 digitos
-            // readListaCodigos .. equals()        
-            //la segunda <= 12 digitos, minimo ¿8?
-            String codigo = tlf.substring(0, 2);
-            String numTlf = tlf.substring(3, tlf.length());
-            int tlfSize = numTlf.length();
-
-            try {
-                Integer.valueOf(numTlf);
-            } catch (NumberFormatException e) {
-
-            }
-        }         
-        return tlf.matches("[0-9]{1,12}");
+            return false;
+        }
     }
 
-    public boolean flyCodValid(String code) {
-        
-         if (comCodValid(code.substring(0, 1))){
-             String codVuelo = code.substring(2, code.length());
-             return codVuelo.matches("[%d]{1,4}");
-         }  else{
-                     return false;
-         }   
+    public boolean isSpanish(String codMunicipio) {
+        return codMunicipio.matches("[%d]{5}");
     }
 
-    public boolean operatingDaysValid() {
-        return true;
+    public boolean isOperatingDaysValid(String days) {
+        if (days != null && days.length() <= 7) {
+            return days.matches(
+                    "[L]? "
+                    + "[M]? "
+                    + "[X]? "
+                    + "[J]? "
+                    + "[V]? "
+                    + "[S]? "
+                    + "[D]? "
+                    + "{1,7}");
+        } else {
+            return false;
+        }
+    }
+
+    public boolean isFechaOperatingDay(Date fecha, String operatingDays) {
+        Format f = new SimpleDateFormat("E");
+        if (operatingDays.contains(f.format(fecha))) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
