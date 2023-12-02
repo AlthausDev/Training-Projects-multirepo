d931cfd3e3ff93d777c90ffd7843391490953223
AlthausDev <samuelalthaus@gmail.com>
2023-12-02 23:44:17 +0100

    feat: add constansts files

--- src/main/java/pa/althaus/dam/javaproyect/aeropuerto/util/config/DateFormats.java
+++ src/main/java/pa/althaus/dam/javaproyect/aeropuerto/util/config/DateFormats.java
@@ -1,6 +1,8 @@
 package pa.althaus.dam.javaproyect.aeropuerto.util.config;
 
+import java.text.SimpleDateFormat;
+
 public class DateFormats {
-    public static final String DATE_FORMAT = "yyyy-MM-dd";
-    public static final String TIME_FORMAT = "HH:mm:ss";
+    public static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd");
+    public static final SimpleDateFormat TIME_FORMAT = new SimpleDateFormat("HH:mm:ss");
 }
--- src/main/java/pa/althaus/dam/javaproyect/aeropuerto/util/config/Paths.java
+++ src/main/java/pa/althaus/dam/javaproyect/aeropuerto/util/config/Paths.java
@@ -4,8 +4,9 @@ package pa.althaus.dam.javaproyect.aeropuerto.util.config;
  * @author S.Althaus
  */
 public class Paths {
-    protected static final String PATH_FLIGH = "aeropuerto/data/Flight.csv";
-    protected static final String PATH_AIRPORTS = "aeropuerto/data/Airport.csv";
-    protected static final String PATH_AIRLINECOMPANY = "aeropuerto/data/AirlineCompany.csv";
+    public static final String PATH_DAILYFLIGH = "aeropuerto/data/DailyFlight.csv";
+    public static final String PATH_FLIGH = "aeropuerto/data/Flight.csv";
+    public static final String PATH_AIRPORTS = "aeropuerto/data/Airport.csv";
+    public static final String PATH_AIRLINECOMPANY = "aeropuerto/data/AirlineCompany.csv";
     public static final String PATH_MUNICIPIOS = "aeropuerto/resources/Municipios.csv";
 }
