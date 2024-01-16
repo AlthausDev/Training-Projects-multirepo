4dc4bf33ee4a67d9073dcd01cca115e32fe7b1e4
AlthausDev <samuelalthaus@gmail.com>
2024-01-16 20:17:23 +0100

    fix: Arreglada estructura inicial de la segunda versión de Roller Dice

---
+++ ATP.12-DiceRoller/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "ATP.12-DiceRoller"
+include(":app")
+ 
