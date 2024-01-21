9d645c49a5740f33a3cc98ec828a7f135b85bf34
AlthausDev <samuelalthaus@gmail.com>
2024-01-21 02:29:08 +0100

    chore: Ajustadas las dimensiones y posiciones de los elementos gráficos, limpieza de codigo

--- ATP.12-DiceRoller/app/src/main/java/com/althaus/dev/atp12_diceroller/usecase/diceRoll/MainActivity.kt
+++ ATP.12-DiceRoller/app/src/main/java/com/althaus/dev/atp12_diceroller/usecase/diceRoll/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import com.althaus.dev.atp12_diceroller.R
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
