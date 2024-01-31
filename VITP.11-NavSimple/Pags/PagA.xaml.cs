b6d7616e13a958d24382a3b4e9c60069d99b83cc
AlthausDev <samuelalthaus@gmail.com>
2024-01-31 21:37:11 +0100

    feat: Agregado pequeño programa de muestra con ViewModel y navegación entre páginas

---
+++ VITP.11-NavSimple/Pags/PagA.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VITP._11_NavSimple.Pags
+{
+    /// <summary>
+    /// Lógica de interacción para PagA.xaml
+    /// </summary>
+    public partial class PagA : UserControl
+    {
+        public PagA()
+        {
+            InitializeComponent();
+        }
+    }
+}
