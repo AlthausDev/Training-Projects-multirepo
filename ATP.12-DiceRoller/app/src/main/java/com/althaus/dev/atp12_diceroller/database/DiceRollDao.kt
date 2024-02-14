539e2ee64b293274f332096c478c6a44caaabc23
AlthausDev <samuelalthaus@gmail.com>
2024-02-14 10:53:16 +0100

    feat: Añadida infreaestructura de Room

---
+++ ATP.12-DiceRoller/app/src/main/java/com/althaus/dev/atp12_diceroller/database/DiceRollDao.kt
@@ -0,0 +1,18 @@
+package com.althaus.dev.atp12_diceroller.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.althaus.dev.atp12_diceroller.model.DiceRoll
+
+@Dao
+interface DiceRollDao {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(diceRoll: DiceRoll)
+
+    @Query("SELECT * FROM dice_roll_table")
+    fun getAllDiceRolls(): LiveData<List<DiceRoll>>
+}
---
+++ ATP.12-DiceRoller/app/src/main/java/com/althaus/dev/atp12_diceroller/model/DiceRoll.kt
@@ -0,0 +1,12 @@
+package com.althaus.dev.atp12_diceroller.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "dice_roll_table")
+data class DiceRoll(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0L,
+    val dice1Result: Int,
+    val dice2Result: Int
+)
