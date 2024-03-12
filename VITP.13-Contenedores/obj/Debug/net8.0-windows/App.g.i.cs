26fe4b00ab178f8ce50895309b1a955e34883b32
AlthausDev <samuelalthaus@gmail.com>
2024-03-12 06:39:01 +0100

    chore: Limpieza

--- VITP.13-Contenedores/obj/Debug/net8.0-windows/App.g.i.cs
+++ VITP.13-Contenedores/obj/Debug/net8.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace Examen5AAS {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.2.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.1.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace Examen5AAS {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.2.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.1.0")]
         public static void Main() {
             Examen5AAS.App app = new Examen5AAS.App();
             app.InitializeComponent();
