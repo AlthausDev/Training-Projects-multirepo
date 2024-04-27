0c22ca0a9d72ac9729cda9f14676d742af056468
AlthausDev <samuelalthaus@gmail.com>
2024-04-27 21:11:04 +0200

    Refactor: Refactorización fase 1

--- C#-GUI-Frameworks/BlazorWebPage/BlazorWebPage/Server/Program.cs
+++ C#-GUI-Frameworks/BlazorWebPage/BlazorWebPage/Server/Program.cs
@@ -14,6 +14,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton();
+
 builder.Services.AddTransient<ITareaRepository, TareaRepository>();
 builder.Services.AddTransient<IUserRepository, UserRepository>();
 
@@ -24,23 +27,22 @@ builder.Services.AddTransient<IUserService, UserService>();
 
 builder.Services.AddRazorPages();
 
+// Configurar la aplicación
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configurar el pipeline de solicitudes HTTP.
 if (app.Environment.IsDevelopment())
 {
     app.UseWebAssemblyDebugging();
-   
+
 }
 else
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
 app.UseBlazorFrameworkFiles();
 app.UseStaticFiles();
 app.UseRouting();
--- C#-GUI-Frameworks/BlazorWebPage/BlazorWebPage/Server/Repository/Impl/TareaRepository.cs
+++ C#-GUI-Frameworks/BlazorWebPage/BlazorWebPage/Server/Repository/Impl/TareaRepository.cs
@@ -2,66 +2,63 @@
 using BlazorWebPage.Shared;
 using Dapper;
 using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
 using System.Data;
 
 namespace BlazorWebPage.Server.Repository.Impl
 {
     public class TareaRepository : ITareaRepository
     {
+        private readonly IConfiguration _configuration;
+        private string ConnectionString => _configuration.GetConnectionString("BlazorWebPageDB");
 
+        public TareaRepository(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
 
         public void Add(Tarea entity)
         {
-            using (IDbConnection dbConnection = new SqlConnection(conexion))
+            using (IDbConnection dbConnection = new SqlConnection(ConnectionString))
             {
-                string query = @"INSERT INTO Tarea VALUES @Tarea;";
-
-                dbConnection.Execute(query);
+                string query = @"INSERT INTO Tarea (Nombre, Descripcion, Finalizado) VALUES (@Nombre, @Descripcion, @Finalizado);";
+                dbConnection.Execute(query, entity);
             }
         }
 
-
         public IEnumerable<Tarea> GetAll()
         {
-            using (IDbConnection dbConnection = new SqlConnection(conexion))
+            using (IDbConnection dbConnection = new SqlConnection(ConnectionString))
             {
                 string query = @"SELECT * FROM Tarea;";
-
                 return dbConnection.Query<Tarea>(query);
             }
         }
 
         public Tarea GetById(int id)
         {
-            using (IDbConnection dbConnection = new SqlConnection(conexion))
+            using (IDbConnection dbConnection = new SqlConnection(ConnectionString))
             {
-                string query = @"SELECT t
-                                 FROM Tarea t
-                                 WHERE t.Id == @id;";
-
-                return (Tarea)dbConnection.Query<Tarea>(query);
+                string query = @"SELECT * FROM Tarea WHERE Id = @id;";
+                return dbConnection.QueryFirstOrDefault<Tarea>(query, new { id });
             }
         }
 
         public void Remove(Tarea entity)
         {
-            using (IDbConnection dbConnection = new SqlConnection(conexion))
+            using (IDbConnection dbConnection = new SqlConnection(ConnectionString))
             {
-                string query = @"DELETE FROM Tarea t
-                                 WHERE t.Id = @entity.Id;";
-
-                dbConnection.Execute(query);
+                string query = @"DELETE FROM Tarea WHERE Id = @Id;";
+                dbConnection.Execute(query, new { entity.Id });
             }
-        }     
+        }
 
         public void Update(Tarea entity)
         {
-            using (IDbConnection dbConnection = new SqlConnection(conexion))
+            using (IDbConnection dbConnection = new SqlConnection(ConnectionString))
             {
-                string query = @"UPDATE t FROM tarea t
-                               WHERE t.Id == @entity.Id;";
-
-               dbConnection.Execute(query);
+                string query = @"UPDATE Tarea SET Nombre = @Nombre, Descripcion = @Descripcion, Finalizado = @Finalizado WHERE Id = @Id;";
+                dbConnection.Execute(query, entity);
             }
         }
     }
