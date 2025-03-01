c1ac4c5925bdfb00a5843fb9d53e932d2317856a
AlthausDev <samuelalthaus@gmail.com>
2025-03-01 14:23:55 +0100

    feat: added some BiPredicate expressions

--- Java/JTP-Lambda/src/main/java/org/althaus/dev/lambda/model/PredicateExample.java
+++ Java/JTP-Lambda/src/main/java/org/althaus/dev/lambda/model/PredicateExample.java
@@ -1,9 +1,9 @@
 package org.althaus.dev.lambda.model;
 
+import java.util.function.BiPredicate;
 import java.util.function.Predicate;
 
 public class PredicateExample {
-
     public static void main(String[] args) {
         Predicate<String> predicate = s -> s.length() > 5;
         System.out.println(predicate.test("Hello"));
@@ -19,5 +19,20 @@ public class PredicateExample {
 
         Predicate<Object> predicate5 = Predicate.isEqual("Hello").and(Predicate.isEqual("World"));
         System.out.println(predicate5.test("Hello"));
+
+        BiPredicate<String, String> t3 = String::equals;
+        System.out.println(t3.test("Hello", "Hello"));
+
+        BiPredicate<String, String> t4 = String::equalsIgnoreCase;
+        System.out.println(t4.test("Hello", "hello"));
+
+        BiPredicate<String, String> t5 = String::startsWith;
+        System.out.println(t5.test("Hello", "H"));
+
+        BiPredicate<Integer, Integer> t6 = Integer::equals;
+        System.out.println(t6.test(5, 5));
+
+        BiPredicate<Integer, Integer> t7 = (i, j) -> i > j;
+        System.out.println(t7.test(5, 6));
     }
 }
